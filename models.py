e0d342564e7d46fb9d9de7dd35aad89833e8551a
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-06-10 17:27:30 -0500

    Volvimos a crear las tablas de BD

--- models.py
+++ models.py
@@ -5,15 +5,15 @@ from database import Base
 class Eficiencia(Base):
     __tablename__ = "eficiencias"
 
-    id                   = Column(Integer, primary_key=True, index=True)
-    nombre_asociado      = Column(String,  nullable=False, index=True)
-    wwid                 = Column(Integer, nullable=False, index=True)
-    linea                = Column(String,  nullable=False, index=True)
-    supervisor           = Column(String,  nullable=False)
-    tipo_proceso         = Column(String,  nullable=False, index=True)  # SW o WD
-    proceso              = Column(String,  nullable=False, index=True)  # Descripción
-    numero_batch         = Column(String,  nullable=False)
-    eficiencia_asociado  = Column(Float,   nullable=False)
-    eficiencia_linea     = Column(Float,   nullable=True)
-    fecha                = Column(Date,    nullable=False, index=True)
-    turno                = Column(String,  nullable=False, index=True)
+    id                 = Column(Integer, primary_key=True, index=True)
+    nombre_asociado    = Column(String, nullable=False, index=True)
+    # wwid            -- ELIMINADO
+    linea              = Column(String, nullable=False, index=True)
+    supervisor         = Column(String, nullable=False)
+    tipo_proceso       = Column(String, nullable=False, index=True)
+    proceso            = Column(String, nullable=False, index=True)
+    # numero_batch    -- ELIMINADO
+    eficiencia_asociado = Column(Float, nullable=False)
+    # eficiencia_linea -- ELIMINADO (la calcularemos en nuestros endpoints)
+    fecha              = Column(Date, nullable=False, index=True)
+    turno              = Column(String, nullable=False, index=True)
