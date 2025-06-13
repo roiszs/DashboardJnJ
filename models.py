c6050ac610717986428fcc8656ea8176e9190136
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-06-12 19:25:10 -0500

    Agregando semana a la bd

--- models.py
+++ models.py
@@ -1,19 +1,16 @@
 # models.py
-from sqlalchemy import Column, Integer, String, Date, Float
+from sqlalchemy import Column, Integer, String, Float
 from database import Base
 
 class Eficiencia(Base):
     __tablename__ = "eficiencias"
 
-    id                 = Column(Integer, primary_key=True, index=True)
-    nombre_asociado    = Column(String, nullable=False, index=True)
-    # wwid            -- ELIMINADO
-    linea              = Column(String, nullable=False, index=True)
-    supervisor         = Column(String, nullable=False)
-    tipo_proceso       = Column(String, nullable=False, index=True)
-    proceso            = Column(String, nullable=False, index=True)
-    # numero_batch    -- ELIMINADO
+    id                  = Column(Integer, primary_key=True, index=True)
+    nombre_asociado     = Column(String, nullable=False, index=True)
+    linea               = Column(String, nullable=False, index=True)
+    supervisor          = Column(String, nullable=False)
+    tipo_proceso        = Column(String, nullable=False, index=True)
+    proceso             = Column(String, nullable=False, index=True)
     eficiencia_asociado = Column(Float, nullable=False)
-    # eficiencia_linea -- ELIMINADO (la calcularemos en nuestros endpoints)
-    fecha              = Column(Date, nullable=False, index=True)
-    turno              = Column(String, nullable=False, index=True)
+    semana              = Column(Integer, nullable=False, index=True)  # NUEVO CAMPO
+    turno               = Column(String, nullable=False, index=True)
