9e17023cedbb690afbcf2a930e798a20b427ffce
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-06-15 19:45:55 -0500

    Agregando fecha

--- models.py
+++ models.py
@@ -1,16 +1,18 @@
 # models.py
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, Date
 from database import Base
+from datetime import date
 
 class Eficiencia(Base):
     __tablename__ = "eficiencias"
 
     id                  = Column(Integer, primary_key=True, index=True)
-    nombre_asociado     = Column(String, nullable=False, index=True)
-    linea               = Column(String, nullable=False, index=True)
-    supervisor          = Column(String, nullable=False)
-    tipo_proceso        = Column(String, nullable=False, index=True)
-    proceso             = Column(String, nullable=False, index=True)
-    eficiencia_asociado = Column(Float, nullable=False)
-    semana              = Column(Integer, nullable=False, index=True)   # ← nuevo
-    turno               = Column(String, nullable=False, index=True)
+    nombre_asociado     = Column(String,  nullable=False, index=True)
+    linea               = Column(String,  nullable=False, index=True)
+    supervisor          = Column(String,  nullable=False)
+    tipo_proceso        = Column(String,  nullable=False, index=True)
+    proceso             = Column(String,  nullable=False, index=True)
+    eficiencia_asociado = Column(Float,   nullable=False)
+    semana              = Column(Integer, nullable=False, index=True)
+    fecha               = Column(Date,    nullable=False, default=date.today, index=True)
+    turno               = Column(String,  nullable=False, index=True)
