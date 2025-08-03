03b14435b466ffc4e74b6798db2b2eecd682d4c7
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-08-03 08:29:48 -0500

    agregando base de datos de usuarios

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # models.py
-from sqlalchemy import Column, Integer, String, Float, Date, text
+from sqlalchemy import Column, Integer, String, Boolean, Float, Date, text
 from database import Base
 from datetime import date
 
@@ -18,3 +18,11 @@ class Eficiencia(Base):
     turno               = Column(String,  nullable=False, index=True)
     piezas              = Column(Integer, nullable=False, default=1)
     tiempo_muerto       = Column(Float,   nullable=False, default=0.0, server_default=text("0.0"), index=True)
+
+class User(Base):
+    __tablename__ = "users"
+    id              = Column(Integer, primary_key=True, index=True)
+    email           = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active       = Column(Boolean, default=True)
+    role            = Column(String, default="viewer")  # "admin" o "viewer"
