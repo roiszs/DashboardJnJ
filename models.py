85cfe454083d6b06ef24e6d4098f78fc51863d77
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-08-07 21:12:02 -0500

    Haciendo cambios en models.py

--- models.py
+++ models.py
@@ -3,9 +3,16 @@ from sqlalchemy import Column, Integer, String, Boolean, Float, Date, text
 from database import Base
 from datetime import date
 
+from fastapi_users.db import SQLAlchemyBaseUserTable  # <— clave
+
+class User(Base, SQLAlchemyBaseUserTable):  # id:int
+    __tablename__ = "users"
+    # Campos estándar ya vienen de SQLAlchemyBaseUserTable:
+    # id (Integer, PK), email, hashed_password, is_active, is_superuser, is_verified
+    role = Column(String, default="viewer", nullable=False)  # tu extra
+
 class Eficiencia(Base):
     __tablename__ = "eficiencias"
-
     id                  = Column(Integer, primary_key=True, index=True)
     nombre_asociado     = Column(String,  nullable=False, index=True)
     linea               = Column(String,  nullable=False, index=True)
@@ -18,11 +25,3 @@ class Eficiencia(Base):
     turno               = Column(String,  nullable=False, index=True)
     piezas              = Column(Integer, nullable=False, default=1)
     tiempo_muerto       = Column(Float,   nullable=False, default=0.0, server_default=text("0.0"), index=True)
-
-#class User(Base):
-    #__tablename__ = "users"
-    #id              = Column(Integer, primary_key=True, index=True)
-    #email           = Column(String, unique=True, index=True, nullable=False)
-    #hashed_password = Column(String, nullable=False)
-    #is_active       = Column(Boolean, default=True)
-    #role            = Column(String, default="viewer")  # "admin" o "viewer"
