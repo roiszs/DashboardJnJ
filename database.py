07337792ebd752fd62327229dab41fea1cd54a24
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-08-07 21:31:26 -0500

    Cambiando archivo databse

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
 # database.py
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-# Para desarrollo local usaremos SQLite:
 SQLALCHEMY_DATABASE_URL = "sqlite:///./eficiencias.db"
 
-# El parámetro connect_args es solo para SQLite
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
