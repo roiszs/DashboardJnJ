f2bc91b14e2f14c938a40124a3bec735c5a41147
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-08-12 20:03:57 -0500

    Cambios en el database

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker as async_sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./eficiencias.db"
 
@@ -10,3 +12,16 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
     pass
+
+ASYNC_SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./eficiencias.db"
+
+async_engine = create_async_engine(
+    ASYNC_SQLALCHEMY_DATABASE_URL,
+    future=True,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
