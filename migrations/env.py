11f469cb87be06685c926727a2520f22ceb99c44
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-09-06 18:27:18 -0500

    Configuraciones

---
+++ migrations/env.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Cargar variables desde .env si existe
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY", "super_secreto")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 2880))
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./eficiencias.db")
+ASYNC_SQLALCHEMY_DATABASE_URL = os.getenv(
+    "ASYNC_SQLALCHEMY_DATABASE_URL",
+    "sqlite+aiosqlite:///./eficiencias.db"
+)
