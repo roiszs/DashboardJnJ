13cb559f2b8eb885d75b56d9960f38ad74d83514
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2025-09-10 19:20:49 -0500

    Editando el archivo de config.py

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
+# config.py
 import os
 from dotenv import load_dotenv
 
+# Cargar variables desde .env si existe
 load_dotenv()
 
+# Variables de configuración
 SECRET_KEY = os.getenv("SECRET_KEY", "super_secreto")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 2880))
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./eficiencias.db")
+
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "SQLALCHEMY_DATABASE_URL",
+    "sqlite:///./eficiencias.db"
+)
+
 ASYNC_SQLALCHEMY_DATABASE_URL = os.getenv(
     "ASYNC_SQLALCHEMY_DATABASE_URL",
     "sqlite+aiosqlite:///./eficiencias.db"
 )
-
